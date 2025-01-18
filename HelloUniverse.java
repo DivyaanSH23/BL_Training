a81ccd1787d2ea8c227e8d983469d31fb28a5fbc
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-01-18 14:58:39 +0530

    initial

---
+++ HelloUniverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class hello
+{
+public static void main(String args[]){
+Scanner sc=new Scanner (System.in);
+int n=sc.nextInt();
+for(int i=0;i<=n;i++){
+if(i%2==0){
+System.out.println(i+" is even no.");
+}
+else
+System.out.println(i+" is odd no.");
+}
+}
+}
